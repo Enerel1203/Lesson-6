b1771292b7d68c531c7fceecbba38a8c62807f2b
Enerel1203 <enerel.bayanmunkh@ris.ae>
2025-07-21 15:03:21 +0400

    Add files via upload

---
+++ Activity 2.py
@@ -0,0 +1,23 @@
+a=4
+b=9
+c=9
+
+print(a != c)
+print(b != c)
+
+a="python"
+b="coding"
+
+if a != b:
+    print(a, "is different from", b)
+
+a=7
+b=4
+
+if (a==0) != (b>5):
+    print("hello")
+
+a=int(input("Enter a number:"))
+
+if a%2 !=0:
+    print("The number is not an even number")
